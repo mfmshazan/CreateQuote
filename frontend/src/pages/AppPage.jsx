5d79db60638a4a46b6f068d48392ea4b5072395b
mfmshazan <shazanmfm.23@uom.lk>
2025-09-30 00:05:46 +0530

    Add state management for quotes and enhance AppPage layout

--- frontend/src/pages/AppPage.jsx
+++ frontend/src/pages/AppPage.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const AppPage = () => {
+
+    const [text, setText] = useState("");
+    const [quotes, setQuotes] = useState([
+        { id: 1, text: "The best way to get started is to quit talking and begin doing." },
+        { id: 2, text: "Don't let yesterday take up too much of today." },
+    ]);
+    const [editId, setEditId] = useState(null);
+    const [editText, setEditText] = useState("");
+
     return (
-        <div>
-            AppPage
+        <div className='min-h-screen bg-gray-900 dark:text-white py-12 px-4 md:px-10'>
+            <h1 className='text-3xl font-bold text-center'>
+                Write Quote
+            </h1>
+
+            <div className=' flex justify-center mt-10'>
+                <input type="text" placeholder='Enter text'
+                className='min-w-xl border-white border-2 px-4 py-2 shadow-lg rounded-lg bg-gray-800'
+                />
+                <button className='ml-4 px-4 py-2 bg-blue-600 text-white rounded-lg shadow-lg hover:bg-blue-700 transition duration-300'>
+                    Add
+                </button>
+            </div>
         </div>
     )
 }
